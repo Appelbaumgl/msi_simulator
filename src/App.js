aa38cfe581d5c821e8d1c1735b5ff3b4ff2dba62
Appelbaumgl <Appelbaumgl@msoe.edu>
2019-02-01 23:56:50 -0600

    Added popup template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import { Button, Dialog, DialogTitle, DialogActions, DialogContent } from '@material-ui/core/';
 import "./App.css"
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
         super();
         this.state = {
             zoom: 15,
+            popupOpen: false,
             Appointment: {
                 Latitude: 43.0440,
                 Longitude: -87.9084,
@@ -30,10 +32,30 @@ class App extends Component {
                     <Marker position={[this.state.ServiceTech.Latitude, this.state.ServiceTech.Longitude]} draggable={true}>
                     </Marker>
                 </Map>
-                <button className={"ChangeDataButton"}>Change Data</button>
+                <button className={"ChangeDataButton"} onClick={this.openPopup.bind(this)}>Change Data</button>
+                <Dialog open={this.state.popupOpen}>
+                    <DialogTitle>Service Tech Info</DialogTitle>
+                    <DialogContent>
+                        <div></div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.closePopup.bind(this)}>
+                            CLOSE
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
+
+    openPopup() {
+        this.setState({ popupOpen: true })
+    }
+
+    closePopup() {
+        this.setState({ popupOpen: false })
+    }
+
 }
 
 export default App;
